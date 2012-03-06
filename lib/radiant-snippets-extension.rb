ab81b93a9e203e74a18992508da2cacf9432c7c2
Jim Gay <jim@saturnflyer.com>
2012-03-06 10:08:23 -0500

    bumping version for clean build in ci

--- lib/radiant-snippets-extension.rb
+++ lib/radiant-snippets-extension.rb
@@ -1,5 +1,5 @@
 module RadiantSnippetsExtension
-  VERSION     = "1.0.0"
+  VERSION     = "1.0.1"
   SUMMARY     = "Snippets for Radiant CMS"
   DESCRIPTION = "Makes Radiant better by adding snippets!"
   URL         = "http://github.com/radiant"
