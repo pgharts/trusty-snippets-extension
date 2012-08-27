f69f9557a8b3e016d22f584f1e3bf85c585eb7cb
Jim Gay <jim@saturnflyer.com>
2012-08-27 16:05:30 -0400

    add a way to find by id/name for snippet files

--- app/models/snippet_finder.rb
+++ app/models/snippet_finder.rb
@@ -1,14 +1,24 @@
 class SnippetFinder
   class << self
+    def find(id)
+      find_map('find',id)
+    end
+
     def find_by_name(name)
-      finder_types.find{|type|
-        found = type.find_by_name(name)
-        return found if found
-      }
+      find_map('find_by_name', name)
     end
 
     def finder_types
       [SnippetFile, Snippet]
+    end
+
+    private
+
+    def find_map(meth, *args)
+      finder_types.find{|type|
+        found = type.send(meth, *args)
+        return found if found
+      }
     end
   end
 end
