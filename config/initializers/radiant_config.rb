90822b33e9c8150dbc5017f167a3a8be1fd6060c
Jim Gay <jim@saturnflyer.com>
2013-09-06 12:34:50 -0400

    only define defaults.snippet.filter if it does not exist

--- config/initializers/radiant_config.rb
+++ config/initializers/radiant_config.rb
@@ -1,3 +1,5 @@
 Radiant.config do |config|
-  config.define 'defaults.snippet.filter', :select_from => lambda { TextFilter.descendants.map { |s| s.filter_name }.sort }, :allow_blank => true
+  if Radiant.config_definitions(key).nil?
+    config.define 'defaults.snippet.filter', :select_from => lambda { TextFilter.descendants.map { |s| s.filter_name }.sort }, :allow_blank => true
+  end
 end
