e071120ade7c9b59f4b62027cdcba480ff525766
Jim Gay <jim@saturnflyer.com>
2013-09-06 12:49:27 -0400

    fix typo in checking key value

--- config/initializers/radiant_config.rb
+++ config/initializers/radiant_config.rb
@@ -1,5 +1,5 @@
 Radiant.config do |config|
-  if Radiant.config_definitions('defaults.snippet.filter').nil?
+  if Radiant.config_definitions['defaults.snippet.filter'].nil?
     config.define 'defaults.snippet.filter', :select_from => lambda { TextFilter.descendants.map { |s| s.filter_name }.sort }, :allow_blank => true
   end
 end
