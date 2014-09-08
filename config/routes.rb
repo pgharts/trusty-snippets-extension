049cedfcc31968e2d8ed842da6cd1b0ae244a79f
Eric Sipple <saalon@gmail.com>
2014-09-08 15:33:38 -0400

    Slight route update.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 TrustyCms::Application.routes.draw do
   namespace :admin do
-    get :remove, on: :member
-    resources :snippets
+
+    resources :snippets do
+      get :remove, on: :member
+    end
     resources :snippet_files, :only => [:index, :show]
   end
 end
