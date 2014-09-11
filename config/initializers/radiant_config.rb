a5930033bead3858429078a03ba00dcc1ecd0c0e
Eric Sipple <saalon@gmail.com>
2014-09-11 09:26:07 -0400

    Changing the radiant config file to actually work.

--- config/initializers/radiant_config.rb
+++ config/initializers/radiant_config.rb
@@ -1,5 +1,3 @@
-TrustyCms::Application.config do |config|
-  if TrustyCms::Application.config_definitions['defaults.snippet.filter'].nil?
-    config.define 'defaults.snippet.filter', :select_from => lambda { TextFilter.descendants.map { |s| s.filter_name }.sort }, :allow_blank => true
-  end
+if TrustyCms.config_definitions['defaults.snippet.filter'].nil?
+  TrustyCms.config.define 'defaults.snippet.filter', :select_from => lambda { TextFilter.descendants.map { |s| s.filter_name }.sort }, :allow_blank => true
 end
