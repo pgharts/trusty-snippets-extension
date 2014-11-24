124274299af6c984ba7a2a0012a056691878a78b
Eric Sipple <saalon@gmail.com>
2014-11-24 10:47:39 -0500

    Updating gemfile to match new versioning.

--- lib/trusty-snippets-extension.rb
+++ lib/trusty-snippets-extension.rb
@@ -1,8 +1,8 @@
 module TrustySnippetsExtension
-  VERSION     = "1.1.3"
-  SUMMARY     = "Snippets for Radiant CMS"
-  DESCRIPTION = "Makes Radiant better by adding snippets!"
-  URL         = "http://github.com/radiant"
-  AUTHORS     = ["Jim Gay"]
-  EMAIL       = ["radiant@radiantcms.org"]
+  VERSION     = "1..0.0"
+  SUMMARY     = "Snippets for Trusty CMS"
+  DESCRIPTION = "Makes Trusty better by adding snippets!"
+  URL         = "http://github.com/pgharts/trusty-snippets-extension"
+  AUTHORS     = ["Jim Gay", "Eric Sipple"]
+  EMAIL       = ["sipple@trustarts.org"]
 end
